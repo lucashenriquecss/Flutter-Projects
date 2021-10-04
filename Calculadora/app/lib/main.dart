6c770fb9d1b05c078d649cc1ca6ad45a16b3f6a9
lucashenriquecss <calheirossaraiva@hotmail.com>
2021-10-04 18:10:27 -0300

    Visual calculadora feito

--- Calculadora/app/lib/main.dart
+++ Calculadora/app/lib/main.dart
@@ -11,7 +11,7 @@ class Calculadora extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Calculadora',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home:  SimpleCalculator(),
+      home: SimpleCalculator(),
     );
   }
 }
@@ -21,20 +21,136 @@ class SimpleCalculator extends StatefulWidget {
   _SimpleCalculatorState createState() => _SimpleCalculatorState();
 }
 
+
+
+
+
 class _SimpleCalculatorState extends State<SimpleCalculator> {
+
+String equation = "0";
+String result = "0";
+String expression = "";
+double equationFontSize = 38.0, resultFontSize = 48.0;
+buttonPressed(String buttonText) {
+  setState(() {
+    if (buttonText == "C") {
+    } else if (buttonText == "<=") {
+    } else if (buttonText == "=") {
+    } else {
+      if(equation =="0"){
+        
+      }
+      equation = equation + buttonText;
+    }
+  });
+}
+
+
+  Widget buildButton(
+      String buttonText, double buttonHeight, Color buttonColor) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.1 * buttonHeight,
+      color: buttonColor,
+      child: FlatButton(
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(0.0),
+            side: BorderSide(
+                color: Colors.white, width: 1, style: BorderStyle.solid)),
+        padding: EdgeInsets.all(16.0),
+        onPressed: () => buttonPressed(buttonText),
+        child: Text(
+          buttonText,
+          style: TextStyle(
+              fontSize: 30.0,
+              fontWeight: FontWeight.normal,
+              color: Colors.white),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Calculadora')),
-      body: Column(
-        children: <Widget>[
-          Container(
-            alignment: Alignment.centerRight,]
-            padding: EdgeInsets.fromLTRB(10,20,10,0),
-            child: Text("0", style: TextStyle(fontsize: 38.0),),
-          )
-        ],
-      )
-    );
+        appBar: AppBar(title: Text('Calculadora')),
+        body: Column(
+          children: <Widget>[
+            Container(
+              alignment: Alignment.centerRight,
+              padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
+              child: Text(
+                equation,
+                style: TextStyle(fontSize: equationFontSize),
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerRight,
+              padding: EdgeInsets.fromLTRB(10, 30, 10, 0),
+              child: Text(
+                result,
+                style: TextStyle(fontSize: resultFontSize),
+              ),
+            ),
+            Expanded(
+              child: Divider(),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  width: MediaQuery.of(context).size.width * .75,
+                  child: Table(
+                    children: [
+                      TableRow(children: [
+                        buildButton("C", 1, Colors.redAccent),
+                        buildButton("<=", 1, Colors.blue),
+                        buildButton("/", 1, Colors.blue),
+                      ]),
+                      TableRow(children: [
+                        buildButton("7", 1, Colors.black54),
+                        buildButton("8", 1, Colors.black54),
+                        buildButton("9", 1, Colors.black54),
+                      ]),
+                      TableRow(children: [
+                        buildButton("4", 1, Colors.black54),
+                        buildButton("5", 1, Colors.black54),
+                        buildButton("6", 1, Colors.black54),
+                      ]),
+                      TableRow(children: [
+                        buildButton("1", 1, Colors.black54),
+                        buildButton("2", 1, Colors.black54),
+                        buildButton("3", 1, Colors.black54),
+                      ]),
+                      TableRow(children: [
+                        buildButton(",", 1, Colors.black54),
+                        buildButton("0", 1, Colors.black54),
+                        buildButton(".", 1, Colors.black54),
+                      ]),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.25,
+                  child: Table(
+                    children: [
+                      TableRow(children: [
+                        buildButton("+", 1, Colors.blue),
+                      ]),
+                      TableRow(children: [
+                        buildButton("*", 1, Colors.blue),
+                      ]),
+                      TableRow(children: [
+                        buildButton("-", 1, Colors.blue),
+                      ]),
+                      TableRow(children: [
+                        buildButton("=", 2, Colors.redAccent),
+                      ]),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ],
+        ));
   }
 }
