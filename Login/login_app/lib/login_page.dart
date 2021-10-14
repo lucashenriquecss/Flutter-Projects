6c7298d0bf6524e2f865d0efe607c8cc1e2f856b
lucashenriquecss <calheirossaraiva@hotmail.com>
2021-10-13 21:09:23 -0300

    Adicionado login, senha, botoes entrar e esqueceu a senha

--- Login/login_app/lib/login_page.dart
+++ Login/login_app/lib/login_page.dart
@@ -28,6 +28,37 @@ class _LoginPageState extends State<LoginPage> {
           border:
               OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
     );
+    final password = TextFormField(
+      autofocus: false,
+      initialValue: '',
+      obscureText: true,
+      decoration: InputDecoration(
+          hintText: 'Senha',
+          contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    );
+
+    final loginButton = Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: Material(
+        borderRadius: BorderRadius.circular(50.0),
+        shadowColor: Colors.lightBlueAccent.shade100,
+        //elevation: 5.0,
+        child: MaterialButton(
+          minWidth: 200.0,
+          height: 42.0,
+          onPressed: () {},
+          color: Colors.lightBlueAccent,
+          child: Text('Entrar', style: TextStyle(color: Colors.white)),
+        ),
+      ),
+    );
+    final forgotLabel = FlatButton(
+      child: Text('Esqueceu a sua senha?',style: TextStyle(color: Colors.black54),
+      ),
+      onPressed: () {},
+    );
     return Scaffold(
       backgroundColor: Colors.white,
       body: Center(
@@ -38,8 +69,11 @@ class _LoginPageState extends State<LoginPage> {
             logo,
             SizedBox(height: 48.0),
             email,
-            SizedBox(height: 8.0),         
-       
+            SizedBox(height: 8.0),
+            password,
+            SizedBox(height: 24.0),
+            loginButton,
+            forgotLabel
           ],
         ),
       ),
