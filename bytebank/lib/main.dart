838919d6de294194867a298aba651a1934552339
lucashenriquecss <calheirossaraiva@hotmail.com>
2021-11-01 18:20:42 -0300

    Reutilizando e stl editor

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -28,47 +28,23 @@ class FormularioTransferencia extends StatelessWidget {
       ),
       body: Column(
         children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 8.0),
-            child: TextField(
-              style: TextStyle(
-                fontSize: 24.0,
-              ),
-              decoration: InputDecoration(
-                  labelText: 'Número da conta', hintText: '0000'),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-              style: TextStyle(
-                fontSize: 24.0,
-              ),
-              decoration: InputDecoration(
-                  icon: Icon(Icons.monetization_on),
-                  labelText: 'Valor',
-                  hintText: '0.00'),
-              keyboardType: TextInputType.number,
-            ),
-          ),
+          Editor(_controladorCampoNumeroConta, 'Numero da conta', '0000'),
+          Editor(_controladorCampoValor, 'Valor', '0.00', Icons.monetization_on),
+          
           ElevatedButton(
             child: Text('Confirmar'),
             onPressed: () {
               debugPrint('clicou no confirmar');
-              final int? numeroConta=int.tryParse(_controladorCampoNumeroConta.text);
-              final double? valor = double.tryParse(_controladorCampoValor.text);
+              final int? numeroConta =
+                  int.tryParse(_controladorCampoNumeroConta.text);
+              final double? valor =
+                  double.tryParse(_controladorCampoValor.text);
 
               if (numeroConta != null && valor != null) {
                 final transferenciaFeita = Transferencia(valor, numeroConta);
                 debugPrint('$transferenciaFeita');
-                 ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text('$transferenciaFeita'),
-                      
-                    ),
-                  );
               }
-            },      
+            },
           ),
         ],
       ),
@@ -76,7 +52,29 @@ class FormularioTransferencia extends StatelessWidget {
   }
 }
 
+class Editor extends StatelessWidget {
+  final TextEditingController controlador;
+  final String rotulo;
+  final String dica;
+  final IconData icone;
 
+  Editor({this.controlador, this.rotulo, this.dica, this.icone});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: TextField(
+        controller: controlador,
+        style: TextStyle(
+          fontSize: 24.0,
+        ),
+        decoration: InputDecoration(icon:Icon(icone),labelText: rotulo, hintText: dica),
+        keyboardType: TextInputType.number,
+      ),
+    );
+  }
+}
 
 class Transferencia {
   final double valor;
